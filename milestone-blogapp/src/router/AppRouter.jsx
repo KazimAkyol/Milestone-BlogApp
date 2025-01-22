0fb5ee306215a8abba433d9f95fe0d2a70848968
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-01-22 12:27:54 +0100

    fourth commit

--- milestone-blogapp/src/router/AppRouter.jsx
+++ milestone-blogapp/src/router/AppRouter.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import Dashboard from "../pages/Dashboard";
 
 const AppRouter = () => {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={""} />
+        <Route path="/" element={<Dashboard />} />
       </Routes>
     </Router>
   );
