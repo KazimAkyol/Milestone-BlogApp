6bad89a8c7f5a8957e87daa1cf9b44fe3f833710
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-01-22 14:55:02 +0100

    seventh commit

--- milestone-blogapp/src/router/AppRouter.jsx
+++ milestone-blogapp/src/router/AppRouter.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Dashboard from "../pages/Dashboard";
 import Login from "../pages/Login";
+import PrivateRouter from "./PrivateRouter";
 
 const AppRouter = () => {
   return (
@@ -9,7 +10,9 @@ const AppRouter = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={<Dashboard />} />
-        <Route path="/auth" element={<Login />} />
+        <Route path="stock" element={<PrivateRouter />}>
+          <Route path="/auth" element={<Login />} />
+        </Route>
       </Routes>
     </Router>
   );
