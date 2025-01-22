3981a03ce162b29b07f7e749031069bb68f36fd5
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-01-22 14:32:55 +0100

    sixth commit

--- milestone-blogapp/src/app/store.jsx
+++ milestone-blogapp/src/app/store.jsx
@@ -1,3 +1,40 @@
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "../features/authSlice";
+import {
+  persistStore,
+  persistReducer,
+  REHYDRATE,
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+import blogReducer from "../features/blogSlice";
 
+const persistConfig = {
+  key: "root",
+  storage,
+};
 
+const persistedReducer = persistReducer(persistConfig, authReducer);
 
+const store = configureStore({
+  reducer: {
+    auth: persistedReducer,
+    stock: blogReducer,
+  },
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+  devTools: process.env.NODE_ENV !== "production",
+});
+
+export let persistor = persistStore(store);
+
+export default store;
---
+++ milestone-blogapp/src/features/blogSlice.jsx
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const blogSlice = createSlice({
+  name: "blog",
+
+  initialState: {
+    loading: false,
+    error: false,
+  },
+
+  reducers: {
+    fetchStart: (state) => {
+      state.loading = true;
+      state.error = false;
+    },
+    fetchFail: (state) => {
+      state.loading = false;
+      state.error = true;
+    },
+  },
+});
+
+export const { fetchStart, fetchFail } = blogSlice.actions;
+
+export default blogSlice.reducer;
